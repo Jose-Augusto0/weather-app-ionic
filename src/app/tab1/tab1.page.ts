fc2a4f73a48c8e7bc320eb33fe8455f9cb09b9bb
José Augusto <joseaugustogomessantos12@gmail.com>
2024-10-14 11:58:48 -0300

    ajustes na view

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,31 +8,38 @@ import { map } from 'rxjs';
   styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page implements OnInit {
-
-  objWeather!:any
-  currentHour!:Date
+  objWeather!: any;
+  currentHour!: Date;
 
   constructor(private service: ServicesService) {
-    this.getCurrentHour()
+    this.getCurrentHour();
   }
 
   ngOnInit(): void {
-    this.service.getWeather('São paulo').pipe(
-      map(el =>{
-        el.main.temp = el.main.temp.toFixed(0);
-        el.main.temp_min = el.main.temp_min.toFixed(0);
-        el.main.temp_max = el.main.temp_max.toFixed(0);
-        el.weather[0].iconURL = `http://openweathermap.org/img/wn/${el.weather[0].icon}.png`; 
-        return el;
-      })
-    ).subscribe(resp => this.objWeather = resp)
-  
+    this.service
+      .getWeather('Recife')
+      .pipe(
+        map((el) => {
+          el.main.temp = el.main.temp.toFixed(0);
+          el.main.temp_min = el.main.temp_min.toFixed(0);
+          el.main.temp_max = el.main.temp_max.toFixed(0);
+          el.weather[0].iconURL = `http://openweathermap.org/img/wn/${el.weather[0].icon}.png`;
+          el.weather[0].description = this.transformDescription(el.weather[0].description);
+          return el;
+        })
+      )
+      .subscribe((resp) => (this.objWeather = resp));
   }
+
+  transformDescription(description: string): string {
+    return (
+      description[0].toUpperCase() + description.slice(1, description.length)
+    );
+  }
+
   getCurrentHour() {
     setInterval(() => {
       this.currentHour = new Date();
     }, 1000);
   }
 }
-
-
